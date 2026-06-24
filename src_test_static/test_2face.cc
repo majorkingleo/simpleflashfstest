a10caa16ae0077b09023a6e0073333eb686d1717
Martin Oberzalek <martin.oberzalek@anton-paar.com>
2026-06-24 07:47:30 +0200

    added tescase for reducing the amount of flashes

--- src_test_static/test_2face.cc
+++ src_test_static/test_2face.cc
@@ -145,6 +145,7 @@ int main( int argc, char **argv )
 		test_cases.push_back( test_case_simple_ini_write_8() );
 		test_cases.push_back( test_case_simple_ini_write_9() );
 		test_cases.push_back( test_case_simple_ini_write_10() );
+		test_cases.push_back( test_case_simple_ini_write_11() );
 
 		test_cases.push_back( test_case_static_TwoFace_write_ini1() );
 
